fb6fa8a062d244316d12e9670657b879f5ac3c4c
Ray2 <RayCogliano1@gmail.com>
2026-03-03 05:11:49 -0500

    new categories

--- content.js
+++ content.js
@@ -2,6 +2,8 @@
 // NOTE: X changes its DOM often. Selectors may need tweaks over time.
 
 const STORAGE_KEY = "timelineFilterSettings";
+const REMOVED_CATEGORIES = new Set(["Indian Subcontinent"]);
+
 const DEFAULTS = {
   mode: "hide", // "hide" | "blur"
   categories: {
@@ -36,6 +38,8 @@ const DEFAULTS = {
         "\\bgonial\\s?angle\\b",
         "\\bfacial\\s?thirds\\b",
         "\\bmidface\\b",
+        "\\bASU\\b",
+        "/\\bClavicular\\b/",
 
         // Community actions/strategies
         "\\bframe\\s?mog\\b",
@@ -48,6 +52,7 @@ const DEFAULTS = {
         "\\bjestergooning\\b",
         "\\bgymcel\\b",
         "\\bmakeupcel\\b",
+        "\\bfiller\\b",
 
         // Controversial practices (high-signal)
         "\\bbone\\s?smash(ing)?\\b",
@@ -65,23 +70,392 @@ const DEFAULTS = {
       patterns: ["\\bwhite\\s*supremac(y|ist)\\b", "\\bneo\\-?nazi\\b", "\\bracial\\s*slur\\b", "\\bnigger\\b", "\\bnigga\\b", "\\bchink\\b", "\\bspic\\b", "\\bkyke\\b", "\\bcracker\\b", "\\bcoon\\b", "\\bgook\\b", "\\btowelhead\\b", "\\bporch monkey\\b", "\\bgo\\s*back\\s*to\\s*your\\s*country\\b", "\\bzipperhead\\b", "\\bbeaner\\b", "\\bwetback\\b", "\\bredneck\\b", "\\btrailer trash\\b", "\\bwhitey\\b", "\\bgringo\\b", "\\bpolack\\b", "\\bdago\\b", "\\bwop\\b", "\\bscalawag\\b", "\\bjew\\b", "\\bgypsy\\b", "\\bgoy\\b"]
     },
 
-    "Indian Subcontinent": { enabled: false, patterns: ["\\bmodi\\b", "\\bindia\\b", "\\bpakistan\\b", "\\bModi\\b",
-"\\bBJP\\b",
-"\\bHindutva\\b",
-"\\bRSS\\b",
-"\\bCAA\\b",
-"\\bNRC\\b",
-"\\bKashmir\\s?(conflict|dispute)\\b",
-"\\bPakistan\\s?Politics\\b",
-"\\bImran\\s?Khan\\b",
-"\\bISI\\b",
-"\\bPTI\\b",
-"\\bPunjab\\s?politics\\b",
-"\\bCross[-\\s]?border\\s?Conflict\\b",
-"\\bIndia\\s?vs\\.?\\s?Pakistan\\b",
-"\\bborder\\s?tensions\\b",
-"\\bLoC\\b",
-"\\bpartition\\s?politics\\b"] },
+    Ads: {
+      enabled: true,
+      patterns: [
+        "\\bsponsored\\b",
+        "\\bpromoted\\b",
+        "\\bpromo\\s?code\\b",
+        "\\baffiliate\\s?link\\b",
+        "\\bpaid\\s?partnership\\b",
+        "\\blink\\s?in\\s?bio\\b",
+        "\\bshop\\s?now\\b",
+        "\\bbuy\\s?now\\b",
+        "\\blimited\\s?time\\s?offer\\b",
+        "\\bad\\b"
+      ]
+    },
+
+    War: {
+      enabled: true,
+      patterns: [
+        "\\biran\\b",
+        "\\bukraine\\b",
+        "\\bisrael\\b",
+        "\\bisraelis\\b",
+        "\\bisraeli\\b",
+        "\\bIsraeli\\b",
+        "\\bIsraelis\\b",
+        "\\bpalestin(e|ian)?\\b",
+        "\\brussia(n)?\\b",
+        "/\\bUS\\b/",
+        "/\\bWW3\\b/",
+        "/\\bAIPAC\\b/",
+        "\\bgaza\\b",
+        "\\bceasefire\\b",
+        "\\bdubai\\b",
+        "\\bDubai?\\b",
+        "\\bAyatollah\\b",
+        "\\bKim\\s?Jong\\b",
+      ]
+    },
+
+    NYC: {
+      enabled: true,
+      patterns: [
+        "\\bnyc\\b",
+        "\\bnew\\s?york\\s?city\\b",
+        "\\bnew\\s?york\\b",
+        "\\bbrooklyn\\b",
+        "\\bqueens\\b",
+        "\\bmanhattan\\b",
+        "\\bharlem\\b",
+        "\\bbronx\\b",
+        "\\bLES\\b",
+        "\\bUWS\\b",
+        "\\bUES\\b",
+         "\\bGreenpoint\\b",
+        "\\west\\s?village\\b",
+        "\\bhells\\s?kitchen\\b",
+        "\\blower\\s?east\\s?side\\b"
+      ]
+    },
+
+    Gym: {
+      enabled: true,
+      patterns: [
+        "\\bgym\\b",
+        "\\bworkout(s)?\\b",
+        "\\bweight\\s?training\\b",
+        "\\blifting\\b",
+        "\\bpowerlifting\\b",
+        "\\bbodybuilding\\b",
+        "\\bhypertrophy\\b",
+        "\\bbench\\s?press\\b",
+        "\\bsquat(s)?\\b",
+        "\\bdeadlift(s|ing)?\\b",
+        "\\bcardio\\b",
+        "\\bbulk(ing)?\\b",
+        "\\bcut(ting)?\\b",
+        "\\bcreatine\\b",
+        "\\bprotein\\s?shake\\b",
+        "\\bpre[-\\s]?workout\\b",
+        "\\bleg\\s?day\\b",
+        "\\bPR\\b"
+      ]
+    },
+
+    Ageism: {
+      enabled: true,
+      patterns: [
+        "\\bzoomers?\\b",
+        "\\bboomers?\\b",
+        "\\bgen\\s?z\\b",
+        "\\bgen\\s?x\\b",
+        "\\bgen\\s?alpha\\b",
+        "\\bgen\\s?y\\b",
+        "\\bmillennials?\\b",
+        "\\bokay\\s?boomer\\b"
+      ]
+    },
+
+    AI: {
+      enabled: true,
+      patterns: [
+        "\\bchat\\s?gpt\\b",
+        "\\banthropic\\b",
+        "\\bclaude\\b",
+        "\\bopenai\\b",
+        "\\bopenclaw\\b",
+        "\\bllm(s)?\\b",
+        "\\bgpt[-\\s]?4\\b",
+        "\\bgpt[-\\s]?5\\b",
+        "\\bgemini\\b",
+        "\\bai\\b",
+        "\\bAi\\b",
+        "\\bAI\\b",
+        "\\bcopilot\\b",
+        "\\ba\\.i\\.\\b"
+      ]
+    },
+
+    "Big Pharma": {
+      enabled: true,
+      patterns: [
+        "\\bpills?\\b",
+        "\\bozempic\\b",
+        "\\btirzapeptide\\b",
+        "\\btirzepatide\\b",
+        "\\bretatutrid\\b",
+        "\\bretatrutide\\b",
+        "\\badvil\\b",
+        "\\bwegrovy\\b",
+        "\\bwegovy\\b",
+        "/\\bSSRI(s)?\\b/",
+        "\\bsemaglutide\\b",
+        "\\bglp[-\\s]?1\\b",
+
+            "\\bOzempic\\b",
+            "\\bWegovy\\b",
+            "\\bSemaglutide\\b",
+            "\\bGLP[-\\s]?1\\b"
+            "\\bCircumcision\\b",
+            "\\bcerebrolysin\\b"    
+
+      ]
+    },
+
+    China: {
+      enabled: true,
+      patterns: [
+        "\\bhot\\s?pot\\b",
+        "\\bhotpot\\b",
+        "\\bchina\\b",
+        "\\bsino\\b",
+        "\\bchinese\\b",
+        "\\bmao\\s?zedong\\b",
+        "\\bxi\\s?jinping\\b",
+        "\\bxi\\s?jingping\\b",
+        "/\\bCCP\\b/",
+        "/\\bPRC\\b/"
+      ]
+    },
+
+    Israel: {
+      enabled: true,
+      patterns: [
+        "\\bjews?\\b",
+        "\\bjewish\\b",
+        "\\bisrael\\b",
+        "\\bnetanyahu\\b",
+        "\\behud\\s?barak\\b",
+        "\\behud\\s?barack\\b",
+        "\\bepstein\\b",
+        "\\bbibi\\b",
+        "\\bgoy\\w*\\b",
+        "\\bisraelis\\b",
+        "\\bisraeli\\b",
+        "\\bIsraeli\\b",
+        "\\bIsraelis\\b",
+      ]
+    },
+
+    "Mental Illness": {
+      enabled: true,
+      patterns: [
+        "\\bautism\\b",
+        "\\bautistic\\b",
+        "/\\bADHD\\b/",
+        "/\\bADD\\b/",
+        "/\\bOCD\\b/",
+        "\\bdepression\\b",
+        "\\banxiety\\b",
+        "\\bbipolar\\b",
+        "\\bschizophreni(a|c)\\b",
+        "/\\bPTSD\\b/",
+        "\\bneurodivergen(t|ce)\\b"
+      ]
+    },
+
+    "Misogyny / Misandry": {
+      enabled: true,
+      patterns: [
+        "\\bmisogyn(y|ist|istic)?\\b",
+        "\\bmisogony\\b",
+        "\\bmisandry\\b",
+        "\\bmisandrist(ic)?\\b",
+        "\\bmen\\b",
+        "\\bguy\\b",
+        "\\bwomen\\b",
+        "\\bboys\\b",
+        "\\bgirls\\b",
+        "\\bwoman\\b",
+        "\\bman\\b",
+        "\\bgirl\\b",
+        "\\bfoid\\b",
+        "\\bfemoid\\b",
+        "\\bincel\\b",
+        "\\bvirgin\\b",
+        "\\bwhore\\b",
+        "\\bslut\\b",
+        "\\bhoe\\b",
+        "\\bbitch\\b",
+        "\\bcunt\\b",
+        "\\bdick\\b",
+        "\\bpussy\\b",
+        "\\bcock\\b",
+            "\\bpenis\\b",        
+      ]
+    },
+
+    Gooning: {
+      enabled: true,
+      patterns: [
+        "\\bthick\\b",
+        "\\bthicc\\b",
+        "\\bbig\\s?ass\\b",
+        "\\btidd(y|ies)\\b",
+        "\\bwaifu\\b",
+        "\\bcock\\b",
+        "\\bbounce\\s?on\\s?it\\b",
+        "\\bhinge\\b",
+        "\\btinder\\b",
+        "\\bonlyfans\\b",
+        "\\blatinas\\b",
+        "\\blatina\\b",
+        "\\basian\\b",
+         "\\basians\\b",
+         "\\bdubai\\b",
+         "\\bDubai\\b",
+         "\\bportapotty\\b",
+        "\\bporn\\b",
+        "\\bpussy\\b",
+        "\\bbaddies\\b",
+        "\\bhotties\\b",
+        "\\bpornhub\\b",
+        "\\binside\\s?her\\b",
+        "\\binside\\s?me\\b",
+        "\\bthirst\\s?trap\\b",
+        "\\bthirst\\s?traps\\b",
+        "\\blink\\s?in\\s?bio\\b",
+        "\\bmy\\s?OF\\b",
+        "\\bOnlyFans\\b",
+        "\\bsub\\s?to\\s?my\\s?page\\b",
+        "\\bDM\\s?for\\s?collab\\b",
+        "\\bnew\\s?pics?\\s?drop\\b",
+        "\\bfishnet(s)?\\b",
+        "\\bplatform\\s?boots?\\b",
+        "\\bheavy\\s?eyeliner\\b",
+        "\\bwinged\\s?liner\\b",
+        "\\bpink\\s?hair\\s?girl\\b",
+        "\\bcosplay\\s?selfie\\b",
+        "\\bmirror\\s?pic\\b",
+        "\\bbody\\s?count\\b",
+        "\\bhot\\b",
+        "\\bvirgin\\b",
+        "\\bsex\\b",
+        "\\bwet\\b",
+        "\\bcum\\b",
+        "\\bdubai\\b",
+        "\\bDubai\\b",
+        "\\bportapotty\\b",
+        "\\bslut\\b",
+        "\\brape\\b",
+        "\\bwhore\\b",
+        "\\bsex\\b",
+        "\\bsexy\\b",
+        "\\bdesire\\b",
+        "\\bdick\\b",
+        "\\bpussy\\b",
+        "\\bcum\\b",
+        "\\bthirst\\b",
+        "\\bthirsty\\b",
+        "\\bthirsttrap\\b",
+        "\\bthirsttraps\\b",
+        "\\blinkinbio\\b",
+        "\\blink\\s?in\\s?bio\\b",
+        "\\bonlyfans\\b",
+        "\\bmyonlyfans\\b",
+        "\\bprostitute\\b",
+        "\\bhooker\\b",
+        "\\bsubscribeto(my)?onlyfans\\b",
+        "\\bdmforcollab(oration)?\\b",
+        "\\bcollab(oration)?inbio\\b",
+        "\\bconsensual\\b",
+        "\\bpedophiles?\\b",
+        "\\bkiss?\\b",
+        "\\bbody\\s?count\\b",
+        "\\bgrooming\\b",
+        "\\bmia\\s?khalifa\\b",
+        "\\bwank\\b",
+        "\\bladyboy\\b",
+      ]
+    },
+      ]
+    },
+
+    Streamers: {
+      enabled: true,
+      patterns: [
+        "\\bclavicular\\b",
+        "\\bspeed\\b",
+        "\\badin\\s?ross\\b",
+        "\\bishowspeed\\b",
+        "\\bkai\\s?cenat\\b",
+        "\\bhasan\\s?piker\\b",
+        "\\bdestiny\\b",
+        "\\bJasontheween\\b",
+        "\\bMarlon\\b",  
+      ]
+    },
+
+    Drugs: {
+      enabled: true,
+      patterns: [
+        "\\bweed\\b",
+        "\\bshrooms?\\b",
+        "\\balcohol\\b",
+        "\\bcocaine\\b",
+        "\\bamphetamines?\\b",
+        "\\bket\\b",
+        "\\bketamine\\b",
+        "/\\bMDMA\\b/"
+      ]
+    },
+
+    "Elon Musk": {
+      enabled: true,
+      patterns: [
+        "\\belon\\s?musk\\b",
+        "\\belon\\b",
+        "\\bmusk\\b",
+        "\\btesla\\b",
+        "\\bspacex\\b",
+        "\\bneuralink\\b",
+        "\\bx\\s?ai\\b",
+        "\\bxai\\b"
+      ]
+    },
+
+    Redpill: {
+      enabled: true,
+      patterns: [
+        "\\bred\\s?pill\\b",
+        "\\bblackpill\\b",
+        "\\bmanosphere\\b",
+        "\\bclavicular\\b",
+        "\\bandrew\\s?tate\\b",
+        "\\bnick\\s?fuentes\\b",
+        "\\balex\\s?jones\\b",
+        "\\bneoliberalism\\b",
+        "\\bliberalism\\b",
+        "\\bconservatism\\b",
+        "\\bimmigrants?\\b",
+      ]
+    },
+
+    "Trad Core": {
+      enabled: true,
+      patterns: [
+        "\\bcigar(s)?\\b",
+        "\\bcigeratte(s)?\\b",
+        "\\bcigarette(s)?\\b",
+        "\\btrad\\s?wife\\b",
+        "\\btradwife\\b",
+        "\\btraditional\\s?wife\\b",
+        "\\bhousewife\\b"
+      ]
+    },
 
     "E-Girls (Pictures)": { enabled: true, patterns: ["\\begirl\\b", "\\bthirst\\s*trap\\b", "\\be[-\\s]?girl(s)?\\b",
 "\\balt\\s?girl\\b",
@@ -167,7 +541,38 @@ const DEFAULTS = {
 "\\bdisrupt(ing|ive)?\\b",
 "\\bgrowth\\s?hacking\\b",
 "\\bmonetize(d|s|ing)?\\b",
-"\\bproduct\\s?market\\s?fit\\b", "\\bopenclaw\\b", "\\bpermanent\\s?underclass"] }
+"\\bproduct\\s?market\\s?fit\\b",
+"/\\bSilicon\\s?Valley\\b/",
+"\\bopenclaw\\b",
+"\\bCEO\\b",
+"\\bC\\s?level\\b",
+"\\bpermanent\\s?underclass"]
+    },
+
+    "Epstein Files": {
+      enabled: true,
+      patterns: [
+        "\\bepstein\\b",
+        "\\bepstein\\s?files\\b",
+        "\\bjeffrey\\s?epstein\\b",
+        "/\\bBill\\s?Clinton\\b/",
+        "/\\bLarry\\s?Summers\\b/",
+        "\\bghislaine\\s?maxwell\\b",
+        "\\blolita\\s?express\\b",
+        "\\bprince\\s?andrew\\b",
+        "\\broyal\\s?family\\b",
+        "\\bpedophile\\b",
+        "\\bsex\\s?trafficking\\b",
+        "\\bunderage\\b",
+        "\\bminor(s)?\\b",
+        "\\bgirls?\\b",
+        "\\bboys?\\b",
+        "\\bvictims?\\b",
+        "\\bsurvivors?\\b"
+        "\\bmaxwell\\b",
+         "\\bClinton\\b",
+      ]
+    }
     }
   }
 };
@@ -189,7 +594,8 @@ function compileSettings(s) {
         // If user wrote something that looks like /.../flags, support it
         const m = p.match(/^\/(.+)\/([gimsuy]*)$/);
         if (m) {
-          regs.push(new RegExp(m[1], m[2] || "i"));
+          // Preserve exact user flags; /.../ without i stays case-sensitive.
+          regs.push(new RegExp(m[1], m[2]));
         } else {
           // Treat as regex string, case-insensitive by default
           regs.push(new RegExp(p, "i"));
@@ -220,10 +626,15 @@ function mergeDefaults(base, override) {
 
   if (override.categories) {
     for (const [k, v] of Object.entries(override.categories)) {
+      if (REMOVED_CATEGORIES.has(k)) continue;
       if (!out.categories[k]) out.categories[k] = { enabled: false, patterns: [] };
       if (typeof v.enabled === "boolean") out.categories[k].enabled = v.enabled;
       if (Array.isArray(v.patterns)) out.categories[k].patterns = v.patterns;
     }
+  }
+
+  for (const k of REMOVED_CATEGORIES) {
+    delete out.categories[k];
   }
   return out;
 }
@@ -233,6 +644,26 @@ function getTweetText(articleEl) {
   return (articleEl.innerText || articleEl.textContent || "").trim();
 }
 
+function clearFilterEffects(articleEl) {
+  articleEl.style.display = "";
+  articleEl.style.filter = "";
+  articleEl.style.opacity = "";
+  articleEl.style.pointerEvents = "";
+
+  const btn = articleEl.querySelector("button[data-tlf-unblur='1']");
+  if (btn) btn.remove();
+}
+
+function resetAllProcessedArticles() {
+  const articles = document.querySelectorAll("article");
+  for (const a of articles) {
+    delete a.dataset.tlfSeen;
+    delete a.dataset.tlfFiltered;
+    delete a.dataset.tlfMatched;
+    clearFilterEffects(a);
+  }
+}
+
 function markFiltered(articleEl, matchedCats) {
   if (articleEl.dataset.tlfFiltered === "1") return;
 
@@ -240,12 +671,14 @@ function markFiltered(articleEl, matchedCats) {
   articleEl.dataset.tlfMatched = matchedCats.join(", ");
 
   if (compiled.mode === "blur") {
+    articleEl.style.display = "";
     articleEl.style.filter = "blur(6px)";
     articleEl.style.opacity = "0.55";
     articleEl.style.pointerEvents = "auto";
 
     // Add small unblur button
     const btn = document.createElement("button");
+    btn.dataset.tlfUnblur = "1";
     btn.textContent = `Unblur (${matchedCats[0]})`;
     btn.style.cssText =
       "position:absolute; z-index:9999; top:8px; right:8px; padding:6px 10px; border-radius:999px; border:1px solid rgba(255,255,255,.2); background:rgba(0,0,0,.65); color:white; cursor:pointer;";
@@ -262,6 +695,7 @@ function markFiltered(articleEl, matchedCats) {
     parent.appendChild(btn);
   } else {
     // Hide mode
+    clearFilterEffects(articleEl);
     articleEl.style.display = "none";
   }
 }
@@ -320,7 +754,8 @@ chrome.storage.onChanged.addListener(async (changes, area) => {
   if (area !== "sync") return;
   if (changes[STORAGE_KEY]) {
     await loadSettings();
-    // Re-scan new content. (Already hidden tweets stay hidden unless you refresh.)
+    // Re-evaluate all currently rendered tweets so popup/options changes apply live.
+    resetAllProcessedArticles();
     scan();
   }
 });
--- popup.js
+++ popup.js
@@ -1,25 +1,315 @@
 const STORAGE_KEY = "timelineFilterSettings";
+const REMOVED_CATEGORIES = new Set(["Indian Subcontinent"]);
+
+const CATEGORY_DEFAULTS = {
+  Mogging: { enabled: true, patterns: ["\\bmog(ging)?\\b", "/\\bClavicular\\b/"] },
+  Racism: { enabled: true, patterns: ["\\bneo\\-?nazi\\b"] },
+  Ads: { enabled: true, patterns: ["\\bsponsored\\b", "\\bpromoted\\b", "\\bad\\b"] },
+  War: {
+    enabled: true,
+    patterns: [
+      "\\biran\\b",
+      "\\bukraine\\b",
+      "\\bisrael\\b",
+      "\\bpalestin(e|ian)?\\b",
+      "\\brussia(n)?\\b",
+      "/\\bUS\\b/",
+      "/\\bWW3\\b/",
+      "/\\bAIPAC\\b/"
+    ]
+  },
+  NYC: {
+    enabled: true,
+    patterns: [
+      "\\bnyc\\b",
+      "\\bnew\\s?york\\s?city\\b",
+      "\\bnew\\s?york\\b",
+      "\\bbrooklyn\\b",
+      "\\bqueens\\b",
+      "\\bmanhattan\\b",
+      "\\bharlem\\b",
+      "\\bbronx\\b",
+      "/\\bLES\\b/",
+      "/\\bUWS\\b/",
+      "/\\bUES\\b/",
+      "\\bhell'?s\\s?kitchen\\b"
+    ]
+  },
+  Gym: {
+    enabled: true,
+    patterns: [
+      "\\bgym\\b",
+      "\\bworkout(s)?\\b",
+      "\\bweight\\s?training\\b",
+      "\\blifting\\b",
+      "\\bpowerlifting\\b",
+      "\\bbodybuilding\\b",
+      "\\bhypertrophy\\b",
+      "\\bbench\\s?press\\b",
+      "\\bsquat(s)?\\b",
+      "\\bdeadlift(s|ing)?\\b",
+      "\\bcardio\\b",
+      "\\bbulk(ing)?\\b",
+      "\\bcut(ting)?\\b",
+      "\\bcreatine\\b",
+      "\\bprotein\\s?shake\\b",
+      "\\bpre[-\\s]?workout\\b",
+      "\\bleg\\s?day\\b",
+      "\\bPR\\b"
+    ]
+  },
+  Ageism: {
+    enabled: true,
+    patterns: [
+      "\\bzoomers?\\b",
+      "\\bboomers?\\b",
+      "\\bgen\\s?z\\b",
+      "\\bgen\\s?x\\b",
+      "\\bgen\\s?alpha\\b",
+      "\\bgen\\s?y\\b",
+      "\\bmillennials?\\b",
+      "\\bokay\\s?boomer\\b"
+    ]
+  },
+  AI: {
+    enabled: true,
+    patterns: [
+      "\\bchat\\s?gpt\\b",
+      "\\banthropic\\b",
+      "\\bclaude\\b",
+      "\\bopenai\\b",
+      "\\bopenclaw\\b",
+      "\\bllm(s)?\\b",
+      "\\bgpt[-\\s]?4\\b",
+      "\\bgpt[-\\s]?5\\b",
+      "\\bgemini\\b",
+      "\\bcopilot\\b"
+    ]
+  },
+  "Big Pharma": {
+    enabled: true,
+    patterns: [
+      "\\bpills?\\b",
+      "\\bozempic\\b",
+      "\\btirzapeptide\\b",
+      "\\btirzepatide\\b",
+      "\\bretatutrid\\b",
+      "\\bretatrutide\\b",
+      "\\badvil\\b",
+      "\\bwegrovy\\b",
+      "\\bwegovy\\b",
+      "/\\bSSRI(s)?\\b/",
+      "\\bsemaglutide\\b",
+      "\\bglp[-\\s]?1\\b"
+    ]
+  },
+  China: {
+    enabled: true,
+    patterns: [
+      "\\bhot\\s?pot\\b",
+      "\\bhotpot\\b",
+      "\\bchina\\b",
+      "\\bsino\\b",
+      "\\bchinese\\b",
+      "\\bmao\\s?zedong\\b",
+      "\\bxi\\s?jinping\\b",
+      "\\bxi\\s?jingping\\b",
+      "/\\bCCP\\b/",
+      "/\\bPRC\\b/"
+    ]
+  },
+  Israel: {
+    enabled: true,
+    patterns: [
+      "\\bjews?\\b",
+      "\\bjewish\\b",
+      "\\bisrael\\b",
+      "\\bnetanyahu\\b",
+      "\\behud\\s?barak\\b",
+      "\\behud\\s?barack\\b",
+      "\\bepstein\\b",
+      "\\bbibi\\b",
+      "\\bgoy\\w*\\b"
+    ]
+  },
+  "Mental Illness": {
+    enabled: true,
+    patterns: [
+      "\\bautism\\b",
+      "\\bautistic\\b",
+      "/\\bADHD\\b/",
+      "/\\bADD\\b/",
+      "/\\bOCD\\b/",
+      "\\bdepression\\b",
+      "\\banxiety\\b",
+      "\\bbipolar\\b",
+      "\\bschizophreni(a|c)\\b",
+      "/\\bPTSD\\b/",
+      "\\bneurodivergen(t|ce)\\b"
+    ]
+  },
+  "Misogyny / Misandry": {
+    enabled: true,
+    patterns: [
+      "\\bmisogyn(y|ist|istic)?\\b",
+      "\\bmisogony\\b",
+      "\\bmisandry\\b",
+      "\\bmisandrist(ic)?\\b",
+      "\\bmen\\b",
+      "\\bwomen\\b",
+      "\\bboys\\b",
+      "\\bgirls\\b",
+      "\\bwoman\\b",
+      "\\bman\\b"
+    ]
+  },
+  Gooning: {
+    enabled: true,
+    patterns: [
+      "\\bthick\\b",
+      "\\bthicc\\b",
+      "\\bbig\\s?ass\\b",
+      "\\btidd(y|ies)\\b",
+      "\\bwaifu\\b",
+      "\\bcock\\b",
+      "\\bbounce\\s?on\\s?it\\b",
+      "\\bhinge\\b",
+      "\\btinder\\b",
+      "\\bonlyfans\\b",
+      "\\bporn\\b",
+      "\\bpornhub\\b"
+    ]
+  },
+  Streamers: {
+    enabled: true,
+    patterns: [
+      "\\bclavicular\\b",
+      "\\bspeed\\b",
+      "\\badin\\s?ross\\b",
+      "\\bishowspeed\\b",
+      "\\bkai\\s?cenat\\b",
+      "\\bhasan\\s?piker\\b",
+      "\\bdestiny\\b"
+    ]
+  },
+  Drugs: {
+    enabled: true,
+    patterns: [
+      "\\bweed\\b",
+      "\\bshrooms?\\b",
+      "\\balcohol\\b",
+      "\\bcocaine\\b",
+      "\\bamphetamines?\\b",
+      "\\bket\\b",
+      "\\bketamine\\b",
+      "/\\bMDMA\\b/"
+    ]
+  },
+  "Elon Musk": {
+    enabled: true,
+    patterns: [
+      "\\belon\\s?musk\\b",
+      "\\belon\\b",
+      "\\bmusk\\b",
+      "\\btesla\\b",
+      "\\bspacex\\b",
+      "\\bneuralink\\b",
+      "\\bx\\s?ai\\b",
+      "\\bxai\\b"
+    ]
+  },
+  Redpill: {
+    enabled: true,
+    patterns: [
+      "\\bred\\s?pill\\b",
+      "\\bblackpill\\b",
+      "\\bmanosphere\\b",
+      "\\bclavicular\\b",
+      "\\bandrew\\s?tate\\b",
+      "\\bnick\\s?fuentes\\b",
+      "\\balex\\s?jones\\b"
+    ]
+  },
+  "Trad Core": {
+    enabled: true,
+    patterns: [
+      "\\bcigar(s)?\\b",
+      "\\bcigeratte(s)?\\b",
+      "\\bcigarette(s)?\\b",
+      "\\btrad\\s?wife\\b",
+      "\\btradwife\\b",
+      "\\btraditional\\s?wife\\b",
+      "\\bhousewife\\b"
+    ]
+  },
+  "E-Girls (Pictures)": { enabled: true, patterns: ["\\begirl\\b"] },
+  "E-Girls (Opinions)": { enabled: false, patterns: [] },
+  "Cat Videos": { enabled: true, patterns: ["\\bcat\\b"] },
+  GamerGate: { enabled: true, patterns: ["\\bgamergate\\b"] },
+  Hyperfinancialization: {
+    enabled: true,
+    patterns: ["\\bcrypto\\b", "/\\bSilicon\\s?Valley\\b/"]
+  },
+  "Epstein Files": {
+    enabled: true,
+    patterns: ["\\bepstein\\b", "/\\bBill\\s?Clinton\\b/", "/\\bLarry\\s?Summers\\b/"]
+  }
+};
 
 const CATEGORY_ORDER = [
   "Mogging",
   "Racism",
-  "Indian Subcontinent",
+  "Ads",
+  "War",
+  "NYC",
+  "Gym",
+  "Ageism",
+  "AI",
+  "Big Pharma",
+  "China",
+  "Israel",
+  "Mental Illness",
+  "Misogyny / Misandry",
+  "Gooning",
+  "Streamers",
+  "Drugs",
+  "Elon Musk",
+  "Redpill",
+  "Trad Core",
   "E-Girls (Pictures)",
   "E-Girls (Opinions)",
   "Cat Videos",
   "GamerGate",
-  "Hyperfinancialization"
+  "Hyperfinancialization",
+  "Epstein Files"
 ];
 
 const ICONS = {
   Mogging: "🫵",
-  Racism: "卐",
-  "Indian Subcontinent": "🗺️",
+  Racism: "🛑",
+  Ads: "📣",
+  War: "🪖",
+  NYC: "🗽",
+  Gym: "🏋️",
+  Ageism: "🧓",
+  AI: "🤖",
+  "Big Pharma": "💊",
+  China: "🇨🇳",
+  Israel: "🇮🇱",
+  "Mental Illness": "🧠",
+  "Misogyny / Misandry": "⚖️",
+  Gooning: "🥵",
+  Streamers: "📺",
+  Drugs: "💉",
+  "Elon Musk": "🚀",
+  Redpill: "🔴",
+  "Trad Core": "🕰️",
   "E-Girls (Pictures)": "🧍‍♀️",
   "E-Girls (Opinions)": "💬",
   "Cat Videos": "🐈",
   GamerGate: "🚫",
-  Hyperfinancialization: "📈"
+  Hyperfinancialization: "📈",
+  "Epstein Files": "🗂️"
 };
 
 async function getSettings() {
@@ -44,6 +334,34 @@ function el(tag, attrs = {}, children = []) {
 
 function deepClone(x) {
   return JSON.parse(JSON.stringify(x ?? {}));
+}
+
+function normalizeSettings(stored) {
+  const out = {
+    mode: stored?.mode === "blur" ? "blur" : "hide",
+    categories: {}
+  };
+
+  const storedCats = stored?.categories || {};
+
+  for (const [name, def] of Object.entries(CATEGORY_DEFAULTS)) {
+    const src = storedCats[name] || {};
+    out.categories[name] = {
+      enabled: typeof src.enabled === "boolean" ? src.enabled : def.enabled,
+      patterns: Array.isArray(src.patterns) ? src.patterns : def.patterns
+    };
+  }
+
+  for (const [name, cat] of Object.entries(storedCats)) {
+    if (REMOVED_CATEGORIES.has(name)) continue;
+    if (name in out.categories) continue;
+    out.categories[name] = {
+      enabled: typeof cat?.enabled === "boolean" ? cat.enabled : false,
+      patterns: Array.isArray(cat?.patterns) ? cat.patterns : []
+    };
+  }
+
+  return out;
 }
 
 function render(settings) {
@@ -89,24 +407,9 @@ function render(settings) {
 }
 
 (async function init() {
-  let settings = await getSettings();
-
-  // If unset, content.js will create defaults internally, but popup needs something to render.
-  // We'll initialize a minimal structure if missing.
-  if (!settings || !settings.categories) {
-    settings = {
-      mode: "hide",
-      categories: {
-        Mogging: { enabled: true, patterns: ["\\bmog(ging)?\\b"] },
-        Racism: { enabled: true, patterns: ["\\bneo\\-?nazi\\b"] },
-        "Indian Subcontinent": { enabled: false, patterns: ["\\bindia\\b"] },
-        "E-Girls (Pictures)": { enabled: true, patterns: ["\\begirl\\b"] },
-        "E-Girls (Opinions)": { enabled: false, patterns: [] },
-        "Cat Videos": { enabled: true, patterns: ["\\bcat\\b"] },
-        GamerGate: { enabled: true, patterns: ["\\bgamergate\\b"] },
-        Hyperfinancialization: { enabled: true, patterns: ["\\bcrypto\\b"] }
-      }
-    };
+  const stored = await getSettings();
+  let settings = normalizeSettings(stored);
+  if (JSON.stringify(stored) !== JSON.stringify(settings)) {
     await setSettings(settings);
   }
 
